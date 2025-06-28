799321b6463f6d389ed073e58b3593178924f8fc
Alwi Alfiansyah Ramdan <alfiansyah.ramdan@gmail.com>
2025-06-29 00:47:25 +0700

    add components for GitHubUserListView

---
+++ GitHubUsers/GitHubUserList/View/GitHubUserListViewItem.swift
@@ -0,0 +1,65 @@
+//
+//  GitHubUserListViewItem.swift
+//  GitHubUsers
+//
+//  Created by Alwi Alfiansyah Ramdan on 28/06/25.
+//
+
+import SwiftUI
+
+struct GitHubUserListViewItem: View {
+  let size: Double
+  let githubUser: GitHubUser
+  
+  var body: some View {
+    VStack {
+      if let avatarURL = githubUser.avatarUrl {
+        let url = URL(string: avatarURL)!
+        AsyncImage(url: url) { image in
+          image
+            .resizable()
+            .scaledToFill()
+            .clipShape(.rect(cornerRadius: 25))
+        } placeholder: {
+          ProgressView()
+            .clipShape(.rect(cornerRadius: 25))
+        }
+        .padding()
+        .frame(width: size, height: size)
+        
+      } else {
+        ProgressView()
+          .clipShape(.rect(cornerRadius: 25))
+          .frame(width: size, height: size)
+      }
+      
+      if let name = githubUser.login {
+        Text(name)
+          .font(.system(size: 18, weight: .bold, design: .monospaced))
+          .foregroundStyle(.black)
+          .lineLimit(1)
+          .allowsTightening(true)
+          .minimumScaleFactor(0.5)
+          .padding(.bottom, 15)
+      }
+    }
+    .frame(width: size, height: size / 0.8)
+    .background(Color.gray.opacity(0.1))
+    .clipShape(.rect(cornerRadius: 25))
+  }
+}
+
+#Preview {
+  let githubUser: GitHubUser = GitHubUser(
+    id: 4660283,
+    login: "aAlwiAlfiansyah",
+    name: "Alwi Alfiansyah Ramdan",
+    avatarUrl: "https://avatars.githubusercontent.com/u/1?v=4",
+    reposUrl: "https://api.github.com/users/aAlwiAlfiansyah/repos",
+    type: "User",
+    followers: 56,
+    following: 12
+  )
+  let size: Double = 150.0
+  return GitHubUserListViewItem(size: size, githubUser: githubUser)
+}
---
+++ GitHubUsers/GitHubUserList/ViewModel/GitHubUserListViewModel.swift
@@ -0,0 +1,64 @@
+//
+//  GitHubUserListViewModel.swift
+//  GitHubUsers
+//
+//  Created by Alwi Alfiansyah Ramdan on 28/06/25.
+//
+
+import Foundation
+
+class GitHubUserListViewModel: ObservableObject {
+  @Published var githubUsers: [GitHubUser]
+  @Published var hasNext: Bool
+  
+  var pagedObject: PagedObject<GitHubUser>?
+  var gitHubUsersAPI: GitHubUsersAPI
+  var userMap: [Int: GitHubUser]
+  
+  init(githubUsersAPI: GitHubUsersAPI) {
+    self.githubUsers = []
+    self.hasNext = false
+    self.userMap = [:]
+    self.gitHubUsersAPI = githubUsersAPI
+    
+    print("GitHubUserListViewModel => created")
+  }
+  
+  deinit {
+    print("GitHubUserListViewModel => destroyed")
+  }
+  
+  func fetchInitialGitHubUserList() async {
+    do {
+      await MainActor.run {
+        self.githubUsers = []
+        self.userMap = [:]
+      }
+      
+      self.pagedObject = try await gitHubUsersAPI.githubUserService.fetchUserList(paginationState: .initial(pageLimit: 30))
+      
+      if let pagedObject = self.pagedObject, let listResult = pagedObject.results {
+        
+        for userResourse in listResult {
+          if userMap[userResourse.id!] == nil {
+            await MainActor.run {
+              self.githubUsers.append(userResourse)
+              self.userMap[userResourse.id!] = userResourse
+            }
+          }
+        }
+        await MainActor.run {
+          self.githubUsers.sort {
+            $0.id! < $1.id!
+          }
+          self.hasNext = pagedObject.hasNext
+        }
+      }
+    } catch {
+      await MainActor.run {
+        self.githubUsers = []
+        self.userMap = [:]
+      }
+    }
+  }
+}
