b06f67cd8ae452f20e55664066a7445e4cbc223a
Alwi Alfiansyah Ramdan <alfiansyah.ramdan@gmail.com>
2025-06-29 23:03:10 +0700

    implement infinite scroll in user repos list view in user detail view page

--- GitHubUsers/GitHubUserDetails/View/GitHubUserDetailsView.swift
+++ GitHubUsers/GitHubUserDetails/View/GitHubUserDetailsView.swift
@@ -9,6 +9,20 @@ import SwiftUI
 
 struct GitHubUserDetailsView: View {
   @ObservedObject var viewModel: GitHubUserDetailsViewModel
+  
+  // Coordinate space for repo list scroll view
+  private let COORDINATE_SPACE: String = "InfiniteReposListScrollContainer"
+  
+  // Bottom offset for loading indicator
+  @State private var bottomOffset: CGFloat?
+  // View height of loading indicator
+  @State private var loadMoreViewHeight: CGFloat?
+  // Lock for loading to prevent multiple calls
+  @State private var loading: Bool = false
+  
+  // Scroll position item id
+  @State var dataID: Int?
+  
   var body: some View {
     VStack {
       GitHubUserInfoView(githubUser: viewModel.githubUser)
@@ -22,7 +36,7 @@ struct GitHubUserDetailsView: View {
           .lineLimit(1)
           .allowsTightening(false)
           .minimumScaleFactor(0.5)
-          .padding(.bottom, 15)
+          .padding(.bottom, 25)
           .padding(.top, 25)
       }
       .frame(minWidth: 0, maxWidth: .infinity)
@@ -37,7 +51,56 @@ struct GitHubUserDetailsView: View {
             }
           }
         }
+        .padding()
+        .scrollTargetLayout()
+        .padding(.bottom, loadMoreViewHeight)
+        .background {
+          // Geometry reader to calculate current scroll view offset
+          // and call onScroll private function
+          GeometryReader { proxy -> Color in
+            onScroll(proxy: proxy)
+            return Color.clear
+          }
+        }
       }
+      .scrollPosition(id: $dataID)
+      .coordinateSpace(name: COORDINATE_SPACE)
+      .overlay(alignment: .bottom) {
+        // Loading indicator
+        // only visible if it's loading and there are something more to be fetched
+        ProgressView()
+          .progressViewStyle(CircularProgressViewStyle())
+          .scaleEffect(1.5)
+          .padding()
+          .tint(.black)
+          .readGeometry {
+            if loadMoreViewHeight != $0.height {
+              loadMoreViewHeight = $0.height
+            }
+          }
+          .offset(y: bottomOffset ?? 1000)
+          .opacity(loading && viewModel.hasNext ? 1 : 0)
+          
+      }
+      .onChange(of: viewModel.githubRepos.count) { oldValue, newValue in
+        if newValue < oldValue {
+          // if newValue is less than oldValue, the list is empty
+          // we need to refetch the user list
+          Task {
+            await viewModel.fetchInitialGitHubUserRepoList()
+            dataID = viewModel.githubRepos.first?.id
+          }
+        } else if loading {
+          // if newValue is more than oldValue, and it's loading, the list has new items
+          // we need to set the loading to false
+          // add sleep to make sure there is no other process
+          Task { @MainActor in
+            try? await Task.sleep(nanoseconds: 100_000_000)
+            loading = false
+          }
+        }
+      }
+      
       Spacer()
     }
     .navigationTitle("GitLab User Details")
@@ -51,7 +114,43 @@ struct GitHubUserDetailsView: View {
     .onAppear {
       Task {
         await viewModel.fetchUserDetailsInfo()
-        await viewModel.fetchInitialGitHubUserRepoList()
+        
+        // Only reloading github user repo list if current list is empty
+        if viewModel.githubRepos.isEmpty {
+          await viewModel.fetchInitialGitHubUserRepoList()
+          dataID = viewModel.githubRepos.first?.id
+        }
+      }
+    }
+  }
+  
+  // Private function for detecting scroll position for infinite scroll.
+  // Will call async function to fetch more if scroll position is at the bottom
+  private func onScroll(proxy: GeometryProxy) {
+    guard let bound = proxy.bounds(of: .named(COORDINATE_SPACE)) else { return }
+
+    let topOffset = bound.minY
+    let contentHeight = proxy.frame(in: .global).height
+    let bottomOffset = contentHeight - bound.maxY
+    
+    Task { @MainActor in
+      if self.bottomOffset != bottomOffset {
+        self.bottomOffset = bottomOffset
+      }
+      
+      if loading { return }
+      
+      if !viewModel.hasNext {
+        loading = false
+        return
+      }
+      
+      // TODO: fix hard code 0.8
+      if let loadMoreViewHeight {
+        if bottomOffset <= loadMoreViewHeight * 0.8 && topOffset >= 0 {
+          loading = true
+          await viewModel.fetchMoreGitHubUserRepoList()
+        }
       }
     }
   }
@@ -61,10 +160,10 @@ struct GitHubUserDetailsView: View {
   let githubUserAPI = GitHubUsersAPI()
   let githubUser: GitHubUser = GitHubUser(
     id: 4660283,
-    login: "aAlwiAlfiansyah",
+    login: "mojombo",
     name: "Alwi Alfiansyah Ramdan",
     avatarUrl: "https://avatars.githubusercontent.com/u/1?v=4",
-    reposUrl: "https://api.github.com/users/aAlwiAlfiansyah/repos",
+    reposUrl: "https://api.github.com/users/mojombo/repos",
     type: "User",
     followers: 56,
     following: 12
